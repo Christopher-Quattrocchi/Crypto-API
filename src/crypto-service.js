f729f9993560ad2272b5e66476b0f2a03f72ef7d
Christopher Quattrocchi <chrisquattrocchi@gmail.com>
2023-11-15 15:17:17 -0800

    basics work, chart works
    Co-authored-by: Ben Reynolds <bgraynolds@gmail.com>

--- src/crypto-service.js
+++ src/crypto-service.js
@@ -1,6 +1,7 @@
-export default class WeatherService {
-  static getWeather(city) {
-    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
+export default class CryptoService {
+  static getCoins(coin) {
+    // console.log("In weatherservice", coin);
+    return fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coin}&order=market_cap_desc&per_page=1&page=1&sparkline=true&price_change_percentage=24h&locale=en`) //Maybe get &precision=2 to work later
       .then(function (response) {
         if (!response.ok) {
           return response.json()
--- src/index.js
+++ src/index.js
@@ -1,40 +1,80 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import WeatherService from './weather-service.js';
+import CryptoService from './crypto-service.js';
+import Chart from 'chart.js';
 
 // Business Logic
 
-function getWeather(city) {
-  WeatherService.getWeather(city)
+function getCoins(coin) {
+  CryptoService.getCoins(coin)
     .then(function (response) {
-      if (response.main) {
-        printElements(response, city);
+      if (response.length > 0 && response[0].id === coin) {
+        printElements(response[0], coin);
       } else {
-        printError(response, city);
+        printError(response, coin);
       }
     });
 }
-// UI Logic
 
-function printElements(response, city) {
-  // console.log("In printElements:", results);
-  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${response.main.humidity}%.
-  The temperature in Kelvins is ${response.main.temp} degrees.`;
+function createChart(dataPoints, coin) {
+  console.log("In createChart, dataPoints", dataPoints);
+  console.log("In createChart, coin", coin);
+  if (!dataPoints || !Array.isArray(dataPoints) || dataPoints.length === 0) {
+    console.error("Invalid or empty dataPoints array");
+    return;
+  }
+
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: Array.from({ length: dataPoints.length}, (_, i) => i),
+      datasets: [{
+        label: `${coin} Price over the last 7 Days`,
+        data: dataPoints,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: false
+        }
+      }
+    }
+  });
 }
 
-function printError(error, city) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${error}.`;
+
+// UI Logic
+
+function printElements(coinData, coin) {
+  // const coinData = response[coin].usd; 
+ console.log("In printElements", coinData);
+ 
+  let sparklineArray = coinData.sparkline_in_7d.price;
+  console.log("In printElements", sparklineArray);
+  
+  document.querySelector('#showResponse').innerText = `Coin is: ${coin}.
+  The exchange rate of your chosen Cryptocurrency is ${coinData.current_price} dollars. The market cap is $${coinData.market_cap}. Trading volume over the last 24 hours is: $${coinData.total_volume}. The change in price over the last 24 hours is %${coinData.price_change_percentage_24h}`; 
+  createChart(sparklineArray, coin);
+}
+
+function printError(error, coin) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the Cryptocurrency data for ${coin}: ${error}.`;
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const city = document.querySelector('#location').value;
-  document.querySelector('#location').value = null;
-  getWeather(city);
+  // debugger;
+  const coin = document.querySelector('#cryptocurrency').value.toLowerCase();
+  
+  console.log(coin);
+  getCoins(coin);
 }
 
 window.addEventListener("load", function () {
   document.querySelector('form').addEventListener("submit", handleFormSubmission);
 });
-// git remote set-url origin https://new.url.here
